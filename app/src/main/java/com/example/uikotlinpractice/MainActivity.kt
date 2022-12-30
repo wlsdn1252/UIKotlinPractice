efcccaa05cc247b36fa0caba6f9ccf8484848072
강진우 <wlsdn1252@naver.com>
2022-12-30 10:35:05 +0900

    입력한 값을 화면에 출력

--- app/src/main/java/com/example/uikotlinpractice/MainActivity.kt
+++ app/src/main/java/com/example/uikotlinpractice/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.uikotlinpractice
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        okBtn.setOnClickListener {
+            //입력된 내용을 변수에 저장(get)
+            val inputContent = contentEdt.text.toString()
+
+            // 저장해둔 입력값을 텍스트뷰의 내용에 반영(set)
+            resultTxt.text = inputContent
+        }
+
     }
 }
